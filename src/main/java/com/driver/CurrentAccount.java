25fe6bc9ba9311864a9a268892ecadf262b7d28f
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-01-13 22:35:50 +0530

    Attempt1

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,18 @@
 package com.driver;
 
+import java.util.ArrayList;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
+    StringBuilder sb;
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        if(balance<5000) {
+            throw new Exception("Insufficient Balance");
+        }
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +20,77 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
+        boolean isval = true;
+        for(int i=1;i<tradeLicenseId.length();i++){
+            if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(i-1)){
+                isval = false;
+            }
+        }
+        if(!isval){
+            String res = rearrange();
+            if(res=="") throw new Exception("Valid License can not be generated");
+        }
     }
+    public  String rearrange(){
+        int N = tradeLicenseId.length();
+        if (N == 0)
+            return "";
 
+        int[] count = new int[26];
+        for (int i = 0; i < 26; i++) {
+            count[i] = 0;
+        }
+        for (char ch : tradeLicenseId.toCharArray()) {
+            count[(int)ch - (int)'a']++;
+        }
+
+        char ch_max = getMaxCountChar(count);
+        int maxCount = count[(int)ch_max - (int)'a'];
+
+        // check if the result is possible or not
+        if (maxCount > (N + 1) / 2)
+            return "";
+
+        String res = "";
+        for (int i = 0; i < N; i++) {
+            res += ' ';
+        }
+
+        int ind = 0;
+        // filling the most frequently occurring char in the
+        // even indices
+        while (maxCount > 0) {
+            res = res.substring(0, ind) + ch_max
+                    + res.substring(ind + 1);
+            ind = ind + 2;
+            maxCount--;
+        }
+        count[(int)ch_max - (int)'a'] = 0;
+
+        // now filling the other Chars, first filling the
+        // even positions and then the odd positions
+        for (int i = 0; i < 26; i++) {
+            while (count[i] > 0) {
+                ind = (ind >= N) ? 1 : ind;
+                res = res.substring(0, ind)
+                        + (char)((int)'a' + i)
+                        + res.substring(ind + 1);
+                ind += 2;
+                count[i]--;
+            }
+        }
+        return res;
+    }
+    public  char getMaxCountChar(int[] count)
+    {
+        int max = 0;
+        char ch = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                ch = (char)((int)'a' + i);
+            }
+        }
+        return ch;
+    }
 }
